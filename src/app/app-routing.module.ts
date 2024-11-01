0d700f227085810b4486a51e07e9873a54884c49
Vivekanand Mendhe <vivek.mendhe.022@gmail.com>
2024-11-01 11:56:46 +0530

    host commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/counterApp', pathMatch: 'full' },
+  {
+    path: 'counterApp',
+    loadChildren: () => {
+      return loadRemoteModule({
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        remoteName: 'remoteApp',
+        exposedModule: './CounterModule',
+      })
+        .then((m) => m.CounterModule)
+        .catch((e) => console.log(e));
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
